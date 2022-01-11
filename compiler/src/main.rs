b35b5344774be7f47a0405d4159820e82e7a4c96
Mike Campbell <mike.campbell@divvypay.com>
2022-01-10 22:41:13 -0700

    iterating

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, path::Path, fmt::format};
 use serde_json::{Result, Value};
 
 fn main() -> Result<()> {
@@ -9,6 +9,16 @@ fn main() -> Result<()> {
     let config: Value = serde_json::from_str(&config_contents)?;
 
     println!("Collection Source: {}", config["collection_source"]);
+
+    let base_path = config["collection_source"].as_str().unwrap();
+    let species_path = format!("{}/species", base_path);
+    print!("{}", base_path);
+
+    let species_filenames = fs::read_dir(species_path).unwrap();
+
+    for path in species_filenames {
+        println!("Name: {}", path.unwrap().path().display())
+    }
 
     Ok(())
 }
