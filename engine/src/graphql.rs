bf74decd6ac1d4ad7c23972c4ec1947c01cb6ac1
Mike Campbell <mike.campbell@divvypay.com>
2022-01-10 17:06:41 -0700

    got a basic monster setup working

--- engine/src/graphql.rs
+++ engine/src/graphql.rs
@@ -1,49 +1,63 @@
-use wasm_bindgen::prelude::*;
+use crate::utils;
+// use wasm_bindgen::prelude::*;
+
 // Only needed due to 2018 edition because the macro is not accessible.
 use juniper::{
   graphql_object, EmptyMutation, FieldResult, 
-  GraphQLEnum, Variables, graphql_value,
+  GraphQLEnum, Variables,
   EmptySubscription
 };
 
-use crate::utils;
-
 #[derive(GraphQLEnum, Clone, Copy)]
-enum Episode {
-    NewHope,
-    Empire,
-    Jedi,
+enum Element {
+    Fire,
+    Water,
+    Grass,
+    Poison,
 }
 
-// Arbitrary context data.
-struct Ctx(Episode);
+#[derive(GraphQLObject)]
+struct Species {
+  id: i32,
+  name: String,
+  elements: Vec<Element>
+}
 
-impl juniper::Context for Ctx {}
+struct Context {
+  // Use your real database pool here.
+  // pool: DatabasePool,
+}
+
+impl juniper::Context for Context {}
 extern crate web_sys;
 
 struct Query;
 
-#[graphql_object(context = Ctx)]
+#[graphql_object(context = Context)]
 impl Query {
-    fn favoriteEpisode(context: &Ctx) -> FieldResult<Episode> {
-        Ok(context.0)
+    fn speciesByID(_context: &Context, id: i32) -> FieldResult<Species> {
+        match id {
+          1 => Ok(Species { id: 1, name: "Bulbasaur".into(), elements: vec![Element::Grass, Element::Poison]}),
+          _ => panic!("no species with id provided")
+        }
     }
 }
 
 // A root schema consists of a query, a mutation, and a subscription.
 // Request queries can be executed against a RootNode.
-type Schema = juniper::RootNode<'static, Query, EmptyMutation<Ctx>, EmptySubscription<Ctx>>;
+type Schema = juniper::RootNode<'static, Query, EmptyMutation<Context>, EmptySubscription<Context>>;
 
 pub fn handle_request(query: &str) -> String {
     utils::set_panic_hook();
 
-    // web_sys::console::log_1(&"Hello, world!".into());
+    web_sys::console::log_1(&"Handling Request: ".into());
+    web_sys::console::log_1(&query.into());
 
       // Create a context object.
-    let ctx = Ctx(Episode::NewHope);
+    let ctx = Context {};
 
     // Run the executor.
-    let (res, errors) = juniper::execute_sync(
+    let (res, _errors) = juniper::execute_sync(
         query,
         None,
         &Schema::new(Query, EmptyMutation::new(), EmptySubscription::new()),
--- smoketest/index.js
+++ smoketest/index.js
@@ -2,5 +2,5 @@ import * as wasm from "omr-engine";
 
 // wasm.greet();
 
-const res = wasm.execute("query { favoriteEpisode }")
+const res = wasm.execute("query { speciesByID(id: 1) { name id elements} }")
 console.log(res)
